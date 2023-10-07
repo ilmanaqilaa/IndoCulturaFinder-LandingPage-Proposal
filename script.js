fcf2e6d4c38de71b671e901163db66cc7e3d1db1
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-10-07 17:11:17 +0700

    Update

--- script.js
+++ script.js
@@ -10,15 +10,92 @@ document.addEventListener("DOMContentLoaded", function () {
             if (pageId === "page1") {
                 const { imageURL, description } = pageData.content;
                 document.querySelector(".container").innerHTML = `
-                <div class="row">
-                <div class="col-md-6">
-                    <img src="${imageURL}" alt="Gambar Perkenalan" class="img-fluid">
-                </div>
-                <div class="col-md-6">
-                    <h1>${pageData.title}</h1>
-                    <p>${description}</p>
-                </div>
-            </div>
+                <style>
+                .container {
+                    width: auto;
+                    display: flex;
+                    margin: 120px 50px;
+                    justify-content: center;
+                }
+                
+                .container.satu {
+                    /* gap: 1rem; */
+                    margin-bottom: 200px;
+                }
+                
+                .atas {
+                    width: 100%;
+                    padding: 20px;
+                    box-sizing: border-box;
+                }
+                
+                .atas.kiri {
+                    /* background-color: aquamarine; */
+                    text-align: center;
+                    margin: auto;
+                }
+                
+                .atas.kiri p {
+                    font-size: large;
+                    font-weight: 700;
+                }
+                
+                .atas.kiri a {
+                    text-decoration: none;
+                }
+                
+                .atas.kiri span {
+                    color: black;
+                }
+                
+                .atas.kiri span:hover {
+                    color: orange;
+                }
+                
+                .atas.kanan a i {
+                    font-size: 400%;
+                    margin: 20px 5px;
+                    color: black;
+                }
+                
+                .fab.fa-github:hover {
+                    color: #2D3E50;
+                }
+                
+                .fab.fa-facebook:hover {
+                    color: #3b5998;
+                }
+                
+                .fab.fa-linkedin:hover {
+                    color: #0e76a8;
+                }
+                
+                .fab.fa-instagram:hover {
+                    color: #C13584;
+                }
+                
+                .atas.kanan {
+                    /* background-color: aqua; */
+                    text-align: center;
+                    flex-grow: 1;
+                }
+                
+                .atas.kanan img {
+                    width:300px;
+                    border-radius: 100%;
+                }
+                </style>
+                <div class="container satu">
+                <article class="atas kiri">
+                  <h1 id="myText">Halo!</h1>
+                  <h2 >Saya Muhammad Ilman Aqilaa</h2>
+                  <p>Saya adalah mahasiswa <a href="https://www.ulbi.ac.id/" target="_blank"><span>Universitas Logistik dan Bisnis Internasional</span></a> Bandung</p>
+                </article>
+        
+                <article id="geografis" class="atas kanan">
+                  <img src="assets/image/ilman.jpg" alt="">
+                </article>
+              </div>
                 `;
             } if (pageId === "page2") {
                 document.querySelector(".container").innerHTML = `
